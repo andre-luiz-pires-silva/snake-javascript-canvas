0998183874a824b5e77f03f79d9e16087ca86b66
Andre Pires <andre.luiz.pires.silva@gmail.com>
2022-04-08 18:32:55 -0300

    Creating 'Grid' class

--- snake.js
+++ snake.js
@@ -1,9 +1,36 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 const canvas_size = 200;
+const cell_size = 20;
 
-canvas.width = canvas_size;
-canvas.height = canvas_size;
+var grid;
 
-context.rect(0, 0, canvas_size, canvas_size);
-context.stroke();
+class Grid {
+  constructor(size) {
+    this.positions = [];
+    for (let column = 0; column < size; column++) {
+      this.positions[column] = [];
+      for (let row = 0; row < size; row++) {
+        this.positions[column][row] = {
+          x: column * cell_size,
+          y: row * cell_size
+        };
+      }
+    }
+  }
+
+  draw = () => {
+    this.positions.flat().forEach(position => context.rect(position.x, position.y, cell_size, cell_size));
+  };
+}
+
+const initialize = () => {
+  canvas.width = canvas_size;
+  canvas.height = canvas_size;
+
+  grid = new Grid(canvas_size / cell_size);
+  grid.draw();
+  context.stroke();
+}
+
+initialize();
